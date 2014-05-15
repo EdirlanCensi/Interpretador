95091f211a7bdacae807e5b554a5b3a5b5f24b07
tbaldin <taiane.baldin@gmail.com>
2014-05-15 07:49:47 -0300

    Pasta Exemplos

--- LeString.java
+++ LeString.java
@@ -2,7 +2,7 @@ class LeString{
 	Fluxo f =new Fluxo();
 	Comando c = new Comando();
 	String linha;
-	int i,valores,;
+	int i,valores;
 	
 	public void busca(String s[]){
 		for(i=0; i < s.length; i++){
@@ -11,31 +11,31 @@ class LeString{
 				
 				if(linha.contains(";")){ //identifica que é uma declaração de variavel
 					
-					Variaveis var = new Variaveis(linha);
+					Variaveis var = new Variaveis();
 					
 				}
 				
-				if(linha.contains("<-"){ //atribuição
+				if(linha.contains("<-")){ //atribuição
 				
 				}
 				
-				if(linha.contains("exibe("){ //printar na tela
-				
-				
-				}
-				
-				if(linha.contains("ler("){ //le do teclado
+				if(linha.contains("exibe(")){ //printar na tela
 				
 				
 				}
 				
-				if(linha.contains("se("){ //controle de fluxo
+				if(linha.contains("ler(")){ //le do teclado
+				
+				
+				}
+				
+				if(linha.contains("se(")){ //controle de fluxo
 				
 				
 				
 				}
 				
-				if(linha.contains("enquanto("){ //laço
+				if(linha.contains("enquanto(")){ //laço
 				
 				
 				
--- Variaveis.java
+++ Variaveis.java
@@ -1,11 +1,13 @@
 class Variaveis{
-	public String nome;
+	public String nome,aux;
 	public double valor;
 	
-	public Variaveis(String s[]){
-		if(s.contains("<-"){
+	public Variaveis(String s){
+		if(s.contains("<-")){
 			this.nome = s.substring(s.indexOf("@")+1, s.indexOf("<-"));
-			this.valor = s.substring(s.indexOf("<-")+1, s.indexOf(";"));
+			aux = s.substring(s.indexOf("<-")+1, s.indexOf(";"));
+			this.valor = Double.parseDouble(aux);
+
 		}else{
 			this.nome = s.substring(s.indexOf("@")+1, s.indexOf(";"));
 			this.valor = 0;		
