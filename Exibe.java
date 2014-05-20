0690bf5cd6fb823a194f9c5b1c326c3866067154
tbaldin <taiane.baldin@gmail.com>
2014-05-20 09:38:28 -0300

    Corrigido erro variaveis
    Variareis bug

--- Exibe.java
+++ Exibe.java
@@ -4,14 +4,14 @@ class Exibe extends LeString{
 		for(int i = 0; i < s.length()-1; i++){
 				if(s.charAt(i) == '*'){
 					aux = s.substring(s.indexOf("*")+1, s.indexOf("!")-1);
-					System.out.println(aux);
+					//System.out.println(aux);
 				}
 				if(s.charAt(i) == '&'){
 					aux = s.substring(s.indexOf("&")+1, s.indexOf("!")-1);
-					System.out.print(indiceVet);
+					//System.out.print(indiceVet);
 					for(int j = 0; j < indiceVet; j++){
 						if(aux == var[j].nome){
-							System.out.print(var[j].valor);
+							//System.out.print(var[j].valor);
 						}
 					}
 				}
--- LeString.java
+++ LeString.java
@@ -2,21 +2,31 @@ class LeString {
 	Fluxo f =new Fluxo();
 	Comando c = new Comando();
 	String linha;
-	int i,valores, indiceVet = 0;
+	int i,valores, indiceVet = -1, teste = 0;;
 	Variaveis var[] = new Variaveis[200];
+	
 	
 	public void busca(String s[]){
 		for(i=0; i < s.length-1; i++){
+			//teste = 0;
 			if(s[i] != null){
 				linha = s[i];
 				linha = linha.replace(" ","");
 				
 				if(linha.contains(";")){ //identifica que é uma declaração de variavel
-					
 					Variaveis v = new Variaveis();
 					v.declaraVar(linha);
-					var[indiceVet] = v;
-					indiceVet++;
+					for(int i=0; i<=indiceVet; i++){
+						if (var[i].nome.equals(v.nome)){
+							var[i].valor = v.valor;
+							teste++;
+							//break;
+						}
+					}
+					if (teste == 0){
+						indiceVet++;
+						var[indiceVet] = v;
+					}						
 				}
 				
 				if(linha.contains("exibe(")){ //printar na tela
@@ -44,7 +54,7 @@ class LeString {
 			}
 				
 		}
-		for(i = 0; i < indiceVet; i++){
+		for(i = 0; i <= var.length(); i++){
 			System.out.println("Nome = "+var[i].nome+"\nValor = "+var[i].valor);
 		}
 	}
